85ef3929e9f92cbd406ac6ec2a4f358078a26e68
wevo16 <wafaaemad16@icloud.com>
2025-08-19 00:02:18 +0300

    refactoring and formatting

--- backend/src/test/java/com/celfocus/hiring/kickstarter/api/CartServiceTest.java
+++ backend/src/test/java/com/celfocus/hiring/kickstarter/api/CartServiceTest.java
@@ -17,7 +17,7 @@ import org.springframework.test.context.ActiveProfiles;
 import java.math.BigDecimal;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest(classes = CartService.class)
@@ -38,39 +38,52 @@ class CartServiceTest {
 
     @Test
     void getCart_shouldThrowCartNotFoundException_whenCartDoesNotExist() {
+        //Given
         String username = "user1";
+        //When
         when(cartRepository.findByUserId(username)).thenReturn(Optional.empty());
-        assertThatThrownBy(() -> cartService.getCart(username))
-            .isInstanceOf(CartNotFoundException.class);
+        //Then
+        assertThrows(CartNotFoundException.class,
+            () -> cartService.getCart(username));
     }
-
     @Test
     void addItemToCart_shouldThrowProductNotFoundException_whenSkuDoesNotExist() {
+        //Given
         String username = "user1";
         String sku = "DOES_NOT_EXIST";
         CartItemInput input = new CartItemInput(sku);
         CartEntity cartEntity = new CartEntity();
         cartEntity.setUserId(username);
+
+        //When
         when(cartRepository.findByUserId(username)).thenReturn(Optional.of(cartEntity));
         when(productRepository.findBySku(sku)).thenReturn(Optional.empty());
-        assertThatThrownBy(() -> cartService.addItemToCart(username, input))
-            .isInstanceOf(ProductNotFoundException.class);
+
+        //Then
+        assertThrows(ProductNotFoundException.class,
+            () -> cartService.addItemToCart(username, input));
     }
 
     @Test
     void addItemToCart_shouldAddItemSuccessfully_whenSkuExists() {
+        // Given
         String username = "user1";
         String sku = "SKU-123";
         CartItemInput input = new CartItemInput(sku);
+
         CartEntity cartEntity = new CartEntity();
         cartEntity.setId(1L);
         cartEntity.setUserId(username);
+
         ProductEntity productEntity = new ProductEntity();
         productEntity.setSku(sku);
         productEntity.setPrice(BigDecimal.valueOf(9.99));
+        //When
         when(cartRepository.findByUserId(username)).thenReturn(Optional.of(cartEntity));
         when(productRepository.findBySku(sku)).thenReturn(Optional.of(productEntity));
+
         cartService.addItemToCart(username, input);
+        //Then
         verify(cartItemRepository, times(1)).save(any(CartItemEntity.class));
     }
 
