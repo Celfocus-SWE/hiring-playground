991c26f49c1dd314763adf64e7b7acc59f70f987
Seifeldein Elkallaf <nb29061@novabase.pt>
2026-02-02 11:41:06 +0400

    Fix - Updated path params to match the new json schema

--- mocks/ids-middleware.js
+++ mocks/ids-middleware.js
@@ -1,17 +1,16 @@
-'use strict'
+"use strict";
 
-const idMappings = { products: "sku", carts: "itemId" };
+const idMappings = { products: "sku", carts: "id" };
 
 module.exports = (req, res, next) => {
-    if (req.method === 'POST' || req.method === 'PUT') {
-        if (req.path.includes('/items')) {
-            console.log('modifying /items id for json-server');
-            req.body.id = req.body[idMappings.carts];
-        }
-        else {
-            console.log('modifying /products id for json-server');
-            req.body.id = req.body[idMappings.products];
-        }
+  if (req.method === "POST" || req.method === "PUT") {
+    if (req.path.includes("/items")) {
+      console.log("modifying /items id for json-server");
+      req.body.id = req.body[idMappings.carts];
+    } else {
+      console.log("modifying /products id for json-server");
+      req.body.id = req.body[idMappings.products];
     }
-    next()
-}
+  }
+  next();
+};
