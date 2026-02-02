7673df5da01b27f6f31c38cbdb6561f0823d64d7
Seifeldein Elkallaf <nb29061@novabase.pt>
2026-02-02 12:27:55 +0400

    Revert - changed ids middleware as well to its default state

--- mocks/ids-middleware.js
+++ mocks/ids-middleware.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const idMappings = { products: "sku", carts: "id" };
+const idMappings = { products: "sku", carts: "itemId" };
 
 module.exports = (req, res, next) => {
   if (req.method === "POST" || req.method === "PUT") {
